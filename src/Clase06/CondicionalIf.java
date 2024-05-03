f837eae05bc79cdeccfb6e8d934bdeae6adc567a
Jorge Salgado <j.salgado2k@gmail.com>
2024-05-03 16:18:08 -0400

    Clase 06

---
+++ src/Clase06/CondicionalIf.java
@@ -0,0 +1,19 @@
+package Clase06;
+
+public class CondicionalIf {
+
+    public static void main(String[] args) {
+
+        // Declaración de variables
+        boolean juegoTerminado = true; // Se utiliza directamente el valor primitivo 'true'
+        int puntuacion = 10000;
+        int nivelCompletado = 8;
+        int bonificacion = 200;
+
+        // Calculo de la puntuación final
+        if (juegoTerminado) {
+            int puntuacionFinal = puntuacion + (nivelCompletado * bonificacion);
+            System.out.println("La puntuación final es = " + puntuacionFinal);
+        }
+    }
+}
