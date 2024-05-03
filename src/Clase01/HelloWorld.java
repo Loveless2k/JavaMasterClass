4192480eab9e1ce8587d91093268f363163b82c1
Jorge Salgado <j.salgado2k@gmail.com>
2024-05-03 11:20:23 -0400

    Clase 01

---
+++ src/Clase01/HelloWorld.java
@@ -0,0 +1,9 @@
+package Clase01;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Imprime el mensaje "Hello World" en la consola
+        System.out.println("Hello World!");
+    }
+}
